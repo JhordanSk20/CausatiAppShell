77dcb45dc52c11884b1301244fc2a5e014bd2d2f
Retuerto Lucero Jhordan Anibal <88565786+JhordanSk20@users.noreply.github.com>
2021-12-06 13:39:36 -0500

    Add files via upload

---
+++ AppCausati/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿
+using AppCausati.Views;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCausati
+{
+    public partial class App : Application
+    {
+     
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage ();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
