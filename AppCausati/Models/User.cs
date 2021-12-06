dfa5e7913909746e7618a5ada20c7fbda3b88ee3
Retuerto Lucero Jhordan Anibal <88565786+JhordanSk20@users.noreply.github.com>
2021-12-06 13:32:32 -0500

    Add files via upload

---
+++ AppCausati/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCausati.Models
+{
+    public class User
+    {
+       public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+
+
+}
+
